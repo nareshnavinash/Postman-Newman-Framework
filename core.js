1d6695a6772aa9222c639a77a84feb5c985eacc8
Naresh Sekar <naresh@madstreetden.com>
2020-07-19 00:49:09 +0530

    sophesticated run

--- core.js
+++ core.js
@@ -16,18 +16,46 @@ class NewmanConfig{
         function parseAndRun(value, index, array) {
             console.log(value)
             console.log(index)
+            console.log(value.collection)
+            console.log(value.environment)
+            if (value.environment == undefined) {
+                NewmanConfig.runCollection(value.collection)
+            } else {
+                NewmanConfig.runCollectionWithEnv(value.collection, value.environment)
+            }
         }
     }
 
-    run(){
+    static runCollectionWithEnv(collection, environment){
         // call newman.run to pass `options` object and wait for callback
+        var file_name = collection.split("/")
         newman.run({
-            collection: require('./collections/file_upload_collection.postman_collection.json'),
-            environment: require('./environment/test_environment.json'),
+            collection: require(collection),
+            environment: require(environment),
             reporters: ['cli', 'json', 'html', 'allure'],
             reporter: {
                 html: {
-                    export: './reports/htmlResults.html' // If not specified, the file will be written to `newman/` in the current working directory.
+                    export: './reports/'.concat(file_name[file_name.length - 1]).concat('.html') // If not specified, the file will be written to `newman/` in the current working directory.
+                },
+                allure: {
+                    export: './reports/allure'
+                }
+            }
+        }, function (err) {
+            if (err) { throw err; }
+            console.log('collection run complete!');
+        });
+    }
+
+    static runCollection(collection){
+        // call newman.run to pass `options` object and wait for callback
+        var file_name = collection.split("/")
+        newman.run({
+            collection: require(collection),
+            reporters: ['cli', 'json', 'html', 'allure'],
+            reporter: {
+                html: {
+                    export: './reports/'.concat(file_name[file_name.length - 1]).concat('.html') // If not specified, the file will be written to `newman/` in the current working directory.
                 },
                 allure: {
                     export: './reports/allure'
