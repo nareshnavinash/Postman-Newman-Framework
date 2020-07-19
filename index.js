d17284ff7ef41e25d144408bd4efb59568e10e63
Naresh Sekar <naresh@madstreetden.com>
2020-07-19 11:14:47 +0530

    updating package and index files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const NewmanConfig = require('./core.js')
 
-var args = process.argv.slice(2);
-
-new NewmanConfig(args[0])
+if (process.argv.length <= 2) {
+    console.log("Give a feed file to proces and run the tests")
+} else {
+    var args = process.argv.slice(2);
+    new NewmanConfig(args[0])
+}
